c6c505eab2d16303b4d805b2bf10e5b018117df4
somso2e <sepi1379@gmail.com>
2025-03-31 00:24:25 +0200

    a

---
+++ htmlpdf/index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--font-render-hinting=none', '--no-sandbox'],
+    headless: "new",
+  });
+  const page = await browser.newPage();
+
+  await page.goto(`file:${path.join(__dirname, '../_pages/cv.html')}`);
+
+  await page.emulateMediaType('screen');
+  await page.evaluate(() => document.body.style.zoom = 0.9);
+
+  const pdf = await page.pdf({
+    path: path.join(__dirname, '../files/Sepand_Soltani_cv.pdf'),
+    margin: { top: '50px', right: '20px', bottom: '50px', left: '20px' },
+    printBackground: true,
+    format: 'A4',
+  });
+
+  await browser.close();
+})();
+
